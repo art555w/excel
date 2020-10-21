bc964bc6589b460314f43337989733acb9a18180
Your Nameart555w <art555m@gmail.com>
2020-10-21 20:16:37 +0500

    finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,87 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsLoader = () => {
+	const loaders = [
+		{
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env']
+			}
+		}
+	]
+	if (isDev) {
+		loaders.push('eslint-loader')
+	}
+
+	return loaders
+}
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	mode: 'development',
-	entry: './index.js',
+	entry: ['@babel/polyfill', './index.js'],
 	output: {
-		filename: 'bundle.js',
+		filename: fileName('js'),
 		path: path.resolve(__dirname, 'dist')
-	}
-
+	},
+	devtool: isDev ? 'source-map' : false,
+	devServer: {
+		port: 9000,
+		hot: isDev
+	},
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@core': path.resolve(__dirname, 'src/core')
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			minify: {
+				removeComments: isProd,
+				collapseWhitespace: isProd
+			}
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/favicon.ico'),
+					to: path.resolve(__dirname, 'dist')
+				},
+			],
+		}),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: fileName('css')
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: jsLoader()
+			},
+		],
+	},
 }
